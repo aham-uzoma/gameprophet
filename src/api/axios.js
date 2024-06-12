be9523ebdc00386352000173b69c002cdb248114
aham-uzoma <ahamefula.uzoma@gmail.com>
2024-06-12 23:34:45 +0100

    protected routes

--- src/api/axios.js
+++ src/api/axios.js
@@ -14,9 +14,11 @@ const BASE_URL = 'http://localhost:8080/api/v1/predict';
 
 export default axios.create({
     baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 
+        'Content-Type': 'application/json',
+        // 'Access-Control-Allow-Origin': 'http://localhost:8080/api/v1/predict'
+    },
     withCredentials: true,
-    followRedirects: true 
 });
 
 // export const axiosPrivate = axios.create({
